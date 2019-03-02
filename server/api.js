005a8b76f49d4d8f851b5ac22df22ccd2949aee2
Boris Makarevitch <boris.makarevitch@gmail.com>
2019-03-02 18:44:21 +0200

    Upgrading angular and route for tracks

--- server/api.js
+++ server/api.js
@@ -1,25 +1,48 @@
 const fs = require('fs'),
       path = require('path'),
-      router = require('express').Router();
+      router = require('express').Router(),
+      media = path.join(__dirname, "..", "media");
 
 router.get('/artists', (req, res) => {
-  var arts = {},
-      root = path.join(__dirname, "..", "media");
+  var arts = {};
 
-  fs.readdirSync(root).forEach(function(el) {
-    let dir = path.join(root, el);
-    
+  fs.readdirSync(media).forEach((location) => {
+    let dir = path.join(media, location);
+
     if (fs.statSync(dir).isFile())
       return;
 
-    fs.readdirSync(dir).forEach(function(name) {
-      arts[name] = el;
+    fs.readdirSync(dir).forEach((name) => {
+      arts[name] = location;
     });
   });
 
  res.json(arts);
 });
-																																																																																																																																																																																																																																																		
+
+router.get('/artists/:path/:artist', (req, res) => {
+  const artPath = path.join(req.params.path, req.params.artist),
+    dir = path.join(media, artPath);
+  
+  var tracks = {}
+
+  fs.readdirSync(dir).forEach((alb) => {
+    let albDir = path.join(dir, alb);
+
+    if (fs.statSync(albDir).isFile())
+      return;  // problem serving mp3 archives
+
+    tracks[alb] = fs.readdirSync(albDir).map((track) => {
+      return {
+        name: track,
+        path: path.join(artPath, alb, track)
+      };
+    });
+  });
+
+  res.json(tracks);
+});
+
 router.get('/', (req, res) => {
   res.status(404).end();
 });
