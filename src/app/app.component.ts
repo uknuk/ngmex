612dffadf63cebb370e016cf6b7dec9fd5dc0e23
Boris Makarevitch <boris.makarevitch@gmail.com>
2019-04-06 14:07:54 +0300

    Changing tabs and playing next

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { ApiService, Track } from './api.service';
 })
 export class AppComponent {
   player: HTMLAudioElement;
+  tab: number;
   api: ApiService;
   arts: Object;
   artNames: Array<string>;
@@ -28,22 +29,24 @@ export class AppComponent {
       this.arts = arts
       this.artNames = Object.keys(arts);
     });
+    this.tab = 0;
   }
 
   ngAfterViewInit() {
     this.player = <HTMLAudioElement> document.getElementById('player');
-
+    this.player.onended = () => this.next();
   }
 
-  public selectArtist(event: any, art: string) {
+  selectArtist(event: any, art: string) {
     this.selArt = art;
     this.api.getTracks(art, this.arts[art]).subscribe(tracks => {
       this.selTracks = tracks;
       this.selAlbs = Object.keys(tracks);
     });
+    this.tab = 0;
   }
 
-  public selectAlbum(event: any, alb: string) {
+  selectAlbum(event: any, alb: string) {
     this.alb = alb;
     this.art = this.selArt;
     this.tracks = this.selTracks[alb];
@@ -51,11 +54,22 @@ export class AppComponent {
     this.play(null, 0);
   }
 
-  public play(event: any, num: number) {
+  play(event: any, num: number) {
     this.trackNum = num;
     const track = this.tracks[num];
     this.track = track.name;
     this.player.src = `${environment.baseUrl}/media?path=${track.path}`;
     this.player.play().catch();
+  }
+
+  next() {
+    let num = this.trackNum + 1;
+    if (num < this.tracks.length)
+      this.play(null, num);
+  }
+
+  setTab($event) {
+    if ($event != this.tab)
+      this.tab = $event;
   }
 }
